c455c12bf42a752a85a1d33a4cd56d9ed4738404
Harry Wyatt <harrywyatt5@outlook.com>
2025-11-05 02:24:59 +0000

    Most of the logic in final state now

--- install_labs.sh
+++ install_labs.sh
@@ -9,6 +9,10 @@ set -e
 # GITHUB_SSH_KEY will use SSH rather than HTTPS to clone Git packages if set, offering that SSH key to GitHub (optional)
 INSTALL_USER="$(logname)"
 SDFORMAT9_BRANCH=''
+GAZEBO_CLASSIC_BRANCH=''
+GAZEBO_PKGS_BRANCH='ros2'
+TURTLEBOT_SIMULATIONS_BRANCH='humble'
+ROS2_CONTROL_BRANCH='humble'
 
 function getGPGKey {
     if [[ -z "${ROS2_GPG_KEY}" ]]; then
@@ -19,7 +23,7 @@ function getGPGKey {
 }
 
 function run_as_install_user {
-    su - "${INSTALL_USER}" -s '/bin/bash' "${1}"
+    su - "${INSTALL_USER}" -s '/bin/bash' -c "${1}"
 }
 
 function getPackagesList {
@@ -48,7 +52,7 @@ function getCoreCount {
     if [[ -z "${USE_CORE_COUNT}" ]]; then
         local total_sys_mem_kbs="$(cat /proc/meminfo | grep 'MemTotal' | awk '{ for(i=1; i<NF; i++) { if($i ~ /[0-9]/) print $i }}')"
         local total_page_mem_kbs="$(cat /proc/meminfo | grep 'SwapTotal' | awk '{ for(i=1; i<NF; i++) { if($i ~ /[0-9]/) print $i }}')"
-        local total_cores="$(($total_sys_mem_kbs + $total_page_mem_kbs) / 2000000)"
+        local total_cores="$((($total_sys_mem_kbs + $total_page_mem_kbs) / 3000000))"
         if [[ "${total_cores}" -eq 0 ]]; then
             echo 1
         elif [[ "${total_cores}" -gt "$(nproc)" ]]; then
@@ -81,7 +85,7 @@ add-apt-repository universe
 apt update
 apt install virt-what curl -y
 
-if ! virt-what; then
+if [[ -z "$(virt-what)" ]]; then
     read -p 'WARN: This system is not a virtual machine. It is highly recommended to run this script in a VM. Press ENTER to continue...'
 fi
 
@@ -90,12 +94,57 @@ getGPGKey > /usr/share/keyrings/ros-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu ${UBUNTU_CODENAME} ${USE_ROS2_BRANCH}" >> /etc/apt/sources.list.d/ros2.list
 apt update
 apt install ros-humble-desktop-full -y
-source /opt/ros/humble/setup.bash
 
 # Install additional packages
 getPackagesList | xargs apt install -y
 
 # Install and build sdformat9
+if [[ -d '/tmp/sdformat9' ]]; then
+    echo '/tmp/sdformat9 already exists' >&2
+    exit 1
+fi
 run_as_install_user 'mkdir /tmp/sdformat9'
 run_as_install_user "git clone $(getGitHubKeySwitch) -b ${SDFORMAT9_BRANCH} $(getGitHubPrefix)gazebosim/sdformat.git /tmp/sdformat9"
 run_as_install_user "mkdir /tmp/sdformat9/build && cd /tmp/sdformat9/build && cmake .. -DCMAKE_INSTALL_PREFIX=/usr/local && make -j$(getCoreCount)"
+cd /tmp/sdformat9/build
+make install
+cd
+# Clean up sdformat9 install location
+rm -r '/tmp/sdformat9'
+
+# Install and build Gazebo Classic
+if [[ -d '/tmp/gc' ]]; then
+    echo '/tmp/gc already exists' >&2
+    exit 1
+fi
+run_as_install_user 'mkdir /tmp/gc'
+run_as_install_user "git clone $(getGitHubKeySwitch) -b ${GAZEBO_CLASSIC_BRANCH} $(getGitHubPrefix)gazebosim/gazebo-classic /tmp/gc"
+run_as_install_user "mkdir /tmp/sdformat9/build && cd /tmp/sdformat9/build && cmake .. -DCMAKE_INSTALL_PREFIX=/usr/local && make -j$(getCoreCount)"
+cd /tmp/gc/build
+make install
+cd
+rm -r /tmp/gc
+
+# Set up user's bashrc
+# TODO: replace /usr/share/gazebo/setup.bash with the real location
+run_as_install_user <<EOF
+echo 'source /opt/ros/humble/setup.bash' >> ~/.bashrc
+echo 'source /usr/share/colcon_cd/function/colcon_cd.sh' >> ~/.bashrc
+echo 'source /usr/share/colcon_argcomplete/hook/colcon-argcomplete.bash' >> ~/.bashrc
+echo 'export _colcon_cd_root=~/ros_additional_libraries' >> ~/.bashrc
+echo 'source /usr/share/colcon_argcomplete/hook/colcon-argcomplete.bash' >> ~/.bashrc
+echo 'source /usr/share/gazebo/setup.bash' >> ~/.bashrc
+EOF
+# TODO: error if already exists
+run_as_install_user <<EOF 
+mkdir ~/ros_additional_libraries
+mkdir ~/ros_additional_libraries/src
+cd ~/ros_additional_libraries/src
+mkdir gazebo_ros_pkgs
+mkdir turtlebot3_simulations
+mkdir gazebo_ros2_control
+EOF
+run_as_install_user "git clone $(getGitHubKeySwitch) -b ${GAZEBO_PKGS_BRANCH} $(getGitHubPrefix)ros-simulation/gazebo_ros_pkgs ~/ros_additional_libraries/src/gazebo_ros_pkgs"
+run_as_install_user "git clone $(getGitHubKeySwitch) -b ${TURTLEBOT_SIMULATIONS_BRANCH} $(getGitHubPrefix)ROBOTIS-GIT/turtlebot3_simulations ~/ros_additional_libraries/src/turtlebot3_simulations"
+run_as_install_user "git clone $(getGitHubKeySwitch) -b ${ROS2_CONTROL_BRANCH} $(getGitHubPrefix)ros-controls/gazebo_ros2_control ~/ros_additional_libraries/src/gazebo_ros2_control"
+run_as_install_user "export MAKEFLAGS='-j$(getCoreCount)' && cd ~/ros_additional_libraries && colcon build --symlink-install"
